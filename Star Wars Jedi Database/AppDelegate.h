0eb4d9a25b873d7065b9ae6a058cd4f2aa48599c
Bruno Dubass <brunodominguezbascones@yahoo.es>
2015-01-08 06:33:21 +0100

    Initial Commit

---
+++ Star Wars Jedi Database/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Star Wars Jedi Database
+//
+//  Created by Bruno Domínguez on 08/01/15.
+//  Copyright (c) 2015 brunodominguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Star Wars Jedi Database/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Star Wars Jedi Database
+//
+//  Created by Bruno Domínguez on 08/01/15.
+//  Copyright (c) 2015 brunodominguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
